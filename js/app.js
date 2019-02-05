53cc14e2a26801af8096b9f8a877f6c7715f93d0
IanNugteren <99045693@mydavinci.nl>
2019-02-05 20:19:08 +0100

    Structuring whole project

--- js/app.js
+++ js/app.js
@@ -1,4 +1,30 @@
-// pro is eens
-// contra is tegen 
-// ambivalent is geen van beide
-console.log("8======D")
+let title = document.getElementById("title");
+let questionDescription = document.getElementById("question-description");
+let hideIntro = document.getElementById("hide-onclick");
+
+document.getElementById("start-button").addEventListener("click", startQuestions);
+document.getElementById("eens").addEventListener("click", eens);
+document.getElementById("none").addEventListener("click", none);
+document.getElementById("oneens").addEventListener("click", onEens);
+
+function startQuestions () 
+{
+    title.innerHTML = subjects[0].title;
+    questionDescription.innerHTML = subjects[0].statement;
+    hideIntro.style.display = 
+}
+
+function none () 
+{
+    console.log('none')
+}
+
+function eens () 
+{
+    console.log('eens');
+}
+
+function onEens () 
+{
+    console.log('oneens');
+}
