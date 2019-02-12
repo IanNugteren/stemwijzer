1f35399d77c709cf0f3cbba559272cbeb78161ec
IanNugteren <99045693@mydavinci.nl>
2019-02-12 20:53:26 +0100

    Gets next question after answering

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,42 @@
-var eens = []
-var oneens = []
-var none = []
+var agreed = []
+var unAgreed = []
+var noChoice = []
 
 const title = document.getElementById("title")
 const questionDescription = document.getElementById("question-description")
 const hideIntro = document.getElementById("hide-onclick")
 const showOptions = document.getElementById("options")
+
 var subjectCount = 0;
 
+window.onload = function () {
+    if (subjectCount === subjects.length) {
+        console.log('lekker man')
+    } else {
+        console.log('niet zo lekker man')
+    }
+}
+
 document.getElementById("start-button").addEventListener("click", startQuestions)
-document.getElementById("eens").addEventListener("click", eens)
-document.getElementById("none").addEventListener("click", none)
-document.getElementById("oneens").addEventListener("click", onEens)
+
+document.getElementById("eens").addEventListener("click", function () {
+    console.log('eens');
+    startQuestions();
+});
+
+document.getElementById("none").addEventListener("click", function () {
+    console.log('none');
+});
+
+document.getElementById("oneens").addEventListener("click", function () {
+    console.log('oneens');
+});
 
 function startQuestions () 
 {
     showOptions.style.display = "block"
-    title.innerHTML = subjects[subjectCount].title
+    title.innerHTML = subjectCount + 1 + ". " + subjects[subjectCount].title
     questionDescription.innerHTML = subjects[subjectCount].statement
     hideIntro.style.display = "none"
-}
-
-function none () 
-{
-    console.log('none')
-}
-
-function eens () 
-{
-
-}
-
-function onEens () 
-{
-    console.log('oneens');
+    subjectCount++
 }
