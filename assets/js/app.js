2eb46178be20b59481a0066cfa7eae00fd69f52c
IanNugteren <99045693@mydavinci.nl>
2019-02-12 23:32:24 +0100

    questionnaire end after 7 questions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,11 @@
 var agreed = []
 var unAgreed = []
 var noChoice = []
-
 const title = document.getElementById("title")
 const questionDescription = document.getElementById("question-description")
 const hideIntro = document.getElementById("hide-onclick")
 const showOptions = document.getElementById("options")
-
 var subjectCount = 0;
-
-window.onload = function () {
-    if (subjectCount === subjects.length) {
-        console.log('lekker man')
-    } else {
-        console.log('niet zo lekker man')
-    }
-}
 
 document.getElementById("start-button").addEventListener("click", startQuestions)
 
@@ -26,17 +16,32 @@ document.getElementById("eens").addEventListener("click", function () {
 
 document.getElementById("none").addEventListener("click", function () {
     console.log('none');
+    startQuestions();
 });
 
 document.getElementById("oneens").addEventListener("click", function () {
     console.log('oneens');
+    startQuestions();
+});
+
+document.getElementById("skip").addEventListener("click", function () {
+    console.log('skipped');
+    startQuestions();
 });
 
 function startQuestions () 
 {
-    showOptions.style.display = "block"
-    title.innerHTML = subjectCount + 1 + ". " + subjects[subjectCount].title
-    questionDescription.innerHTML = subjects[subjectCount].statement
-    hideIntro.style.display = "none"
-    subjectCount++
+    if (subjectCount == subjects.length) {
+        title.innerHTML = "Einde van de vragenlijst"
+        hideIntro.style.display = "none"
+        showOptions.style.display = "block"
+        questionDescription.style.display = "none"
+    
+    } else {
+        showOptions.style.display = "block"
+        title.innerHTML = subjectCount + 1 + ". " + subjects[subjectCount].title
+        questionDescription.innerHTML = subjects[subjectCount].statement
+        hideIntro.style.display = "none"
+        subjectCount++
+    }
 }
